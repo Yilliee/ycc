9b78d0c94818697e76e4aa92e78ce86a8f0ae760
Yillié <yilliee@proton.me>
2025-03-09 16:05:23 +0500

    Preprocessor: Replace comments with spaces, strip trailing whitespaces
    - Instead of removing comments entirely, replace them with
      spaces similar to gcc's preprocessor.
    - Remove all trailing whitespaces in a second pass over the file.

--- preprocessor/preprocessor.cpp
+++ preprocessor/preprocessor.cpp
@@ -1,9 +1,14 @@
-#include "preprocessor.h"
-#include "../filemanager/reader.h"
-#include "../filemanager/writer.h"
+#include <filemanager/reader.h>
+#include <filemanager/writer.h>
+#include <filesystem>
 #include <iostream>
+#include <preprocessor/preprocessor.h>
+#include <queue>
 
 using filemanager::reader, filemanager::writer;
+using std::cerr, std::endl;
+using std::filesystem::path, std::filesystem::remove,
+    std::filesystem::filesystem_error;
 
 inline void skip_till_comment_start(reader &rdr, writer &wrtr,
                                     bool &single_line_comment,
@@ -30,10 +35,15 @@ inline void skip_till_comment_start(reader &rdr, writer &wrtr,
   }
 }
 
-inline void ignore_till_comment_end(reader &rdr, bool &single_line_comment,
+inline void ignore_till_comment_end(reader &rdr, writer &wrtr,
+                                    bool &single_line_comment,
                                     bool &multi_line_comment) {
   while (!rdr.is_finished() && (single_line_comment || multi_line_comment)) {
     char ch = rdr.get_next_char();
+    if (ch != '\n')
+      wrtr.put_next_char(' ');
+    else
+      wrtr.put_next_char('\n');
 
     if (ch == '\n' && single_line_comment)
       single_line_comment = false;
@@ -45,6 +55,20 @@ inline void ignore_till_comment_end(reader &rdr, bool &single_line_comment,
 
 void preprocessor::preprocess_file(const std::string &input_file,
                                    const std::string &output_file) {
+  remove_comments(input_file, TEMP_FILE);
+  remove_trailing_spaces(TEMP_FILE, output_file);
+
+  path to_remove = TEMP_FILE;
+  try {
+    if (!remove(to_remove))
+      cerr << "Unable to remove temporary file of preprocessor\n";
+  } catch (filesystem_error &e) {
+    cerr << "Filesystem error: " << e.what() << endl;
+  }
+}
+
+void preprocessor::remove_comments(const std::string &input_file,
+                                   const std::string &output_file) {
   reader rdr(input_file);
   writer wrtr(output_file);
 
@@ -52,12 +76,43 @@ void preprocessor::preprocess_file(const std::string &input_file,
   while (!rdr.is_finished()) {
     skip_till_comment_start(rdr, wrtr, single_line_comment, multi_line_comment);
 
-    if (single_line_comment)
-      wrtr.put_next_char('\n');
-    ignore_till_comment_end(rdr, single_line_comment, multi_line_comment);
+    ignore_till_comment_end(rdr, wrtr, single_line_comment, multi_line_comment);
 
     if (multi_line_comment)
       std::cerr << "Unterminated multi line comment\n";
+  }
+
+  rdr.close();
+  wrtr.close();
+}
+
+void preprocessor::remove_trailing_spaces(const std::string &input_file,
+                                          const std::string &output_file) {
+  reader rdr(input_file);
+  writer wrtr(output_file);
+
+  if (rdr.is_finished())
+    return;
+
+  std::queue<char> whitespaces;
+  while (!rdr.is_finished()) {
+    char ch = rdr.get_next_char();
+
+    if (ch != '\n' && isspace(ch)) {
+      whitespaces.push(ch);
+      continue;
+    }
+
+    if (ch != '\n') {
+      while (!whitespaces.empty()) {
+        wrtr.put_next_char(whitespaces.front());
+        whitespaces.pop();
+      }
+    } else {
+      while (!whitespaces.empty())
+        whitespaces.pop();
+    }
+    wrtr.put_next_char(ch);
   }
 
   rdr.close();
--- preprocessor/preprocessor.h
+++ preprocessor/preprocessor.h
@@ -5,7 +5,12 @@
 #include <string>
 
 namespace preprocessor {
+const std::string TEMP_FILE = ".temp.i";
 void preprocess_file(const std::string &input_file,
                      const std::string &output_file);
+void remove_comments(const std::string &input_file,
+                     const std::string &output_file);
+void remove_trailing_spaces(const std::string &input_file,
+                            const std::string &output_file);
 } // namespace preprocessor
 #endif /* PREPROCESSOR_H */
